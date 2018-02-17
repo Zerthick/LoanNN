aa5792c9dd3422e88379deca8d4cc821e3cc5d2f
Darren Chan <darrennchan8@Gmail.com>
2018-02-16 23:32:23 -0500

    clean data

--- clean.py
+++ clean.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
-#train_data = np.genfromtxt('credit_train.csv', delimiter=',')
-df = pd.read_csv('credit_train.csv', usecols=[3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18])
-l = pd.read_csv('credit_train.csv', usecols=[2])
-print(df)
-features = train_data[2:]
+def get_clean_data():
+    df = pd.read_csv('credit_train.csv', usecols=[2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18])
+    df = df.dropna()
+    return df
