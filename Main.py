d0f4e3460355c10ad23c410bdb8f206d9f4fa4cc
Zerthick <zerthick@gmail.com>
2018-02-17 07:50:41 -0500

    Change to new dataset

--- Main.py
+++ Main.py
@@ -6,7 +6,8 @@ from sklearn.model_selection import train_test_split
 
 
 def gen_data():
-    df = pd.read_csv('clean_data.csv', usecols=[1, 2, 3, 4, 5, 6, 7], header=0)
+    df = pd.read_csv('clean_data.csv', header=0)
+    df = df.sample(n=100)
 
     train_data, test_data = train_test_split(df, test_size=0.4)
 
@@ -30,19 +31,38 @@ def get_input_fn(x_in, y_in):
 
 def main(argv):
     x_train, y_train, x_test, y_test = gen_data()
-    my_feature_columns = [tf.feature_column.numeric_column('Principal'),
-                          tf.feature_column.numeric_column('terms'),
-                          tf.feature_column.numeric_column('past_due_days'),
-                          tf.feature_column.numeric_column('age'),
-                          tf.feature_column.categorical_column_with_hash_bucket('education', hash_bucket_size=5),
-                          tf.feature_column.categorical_column_with_hash_bucket('Gender', hash_bucket_size=2)]
+    my_feature_columns = [tf.feature_column.numeric_column('loan_amnt'),
+                          tf.feature_column.numeric_column('term'),
+                          tf.feature_column.numeric_column('int_rate'),
+                          tf.feature_column.numeric_column('installment'),
+                          tf.feature_column.categorical_column_with_hash_bucket('grade', hash_bucket_size=26),
+                          tf.feature_column.categorical_column_with_hash_bucket('home_ownership', hash_bucket_size=10),
+                          tf.feature_column.numeric_column('annual_inc'),
+                          tf.feature_column.categorical_column_with_hash_bucket('verification_status',
+                                                                                hash_bucket_size=10),
+                          tf.feature_column.categorical_column_with_hash_bucket('pymnt_plan', hash_bucket_size=10),
+                          tf.feature_column.numeric_column('dti'),
+                          tf.feature_column.numeric_column('delinq_2yrs'),
+                          tf.feature_column.numeric_column('inq_last_6mths'),
+                          tf.feature_column.numeric_column('open_acc'),
+                          tf.feature_column.numeric_column('revol_bal'),
+                          tf.feature_column.numeric_column('revol_util'),
+                          tf.feature_column.numeric_column('total_acc'),
+                          tf.feature_column.numeric_column('out_prncp'),
+                          tf.feature_column.numeric_column('out_prncp_inv'),
+                          tf.feature_column.numeric_column('total_pymnt'),
+                          tf.feature_column.numeric_column('total_pymnt_inv'),
+                          tf.feature_column.numeric_column('total_rec_prncp'),
+                          tf.feature_column.numeric_column('total_rec_int'),
+                          tf.feature_column.numeric_column('total_rec_late_fee'),
+                          tf.feature_column.numeric_column('recoveries'),
+                          tf.feature_column.numeric_column('collection_recovery_fee')]
 
     model_dir = tempfile.mkdtemp()
 
-    model = tf.estimator.LinearClassifier(model_dir=model_dir, feature_columns=my_feature_columns,
-                                          n_classes=3)
+    model = tf.estimator.LinearClassifier(model_dir=model_dir, feature_columns=my_feature_columns, n_classes=6)
 
-    model.train(input_fn=get_input_fn(x_train, y_train), steps=1)
+    model.train(input_fn=get_input_fn(x_train, y_train), steps=5000)
 
     results = model.evaluate(input_fn=get_input_fn(x_test, y_test), steps=None)
 
@@ -50,5 +70,4 @@ def main(argv):
 
 
 if __name__ == '__main__':
-    tf.logging.set_verbosity(tf.logging.INFO)
     tf.app.run(main=main, argv=None)
--- clean.py
+++ clean.py
@@ -1,28 +1,60 @@
-import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
+import pandas as pd
+
 
 def clean_training_data():
-    df = pd.read_csv('Loan payments data.csv', usecols=[1, 2, 3,  7, 8, 9, 10])
-    df['loan_status'] = df['loan_status'].map({'PAIDOFF': 0, 'COLLECTION': 1, 'COLLECTION_PAIDOFF': 2})
-    df['past_due_days'] = df['past_due_days'].apply(lambda x: x if x > 0 else 0)
-    df = df.dropna()
+    df = pd.read_csv('loanfull.csv', usecols=[
+        'loan_amnt',
+        'term',
+        'int_rate',
+        'installment',
+        'grade',
+        'home_ownership',
+        'annual_inc',
+        'verification_status',
+        'loan_status',
+        'pymnt_plan',
+        'dti',  # Debt to income ratio
+        'delinq_2yrs',
+        'inq_last_6mths',
+        'open_acc',
+        'revol_bal',
+        'revol_util',
+        'total_acc',
+        'out_prncp',
+        'out_prncp_inv',
+        'total_pymnt',
+        'total_pymnt_inv',
+        'total_rec_prncp',
+        'total_rec_int',
+        'total_rec_late_fee',
+        'recoveries',
+        'collection_recovery_fee'
+    ])
+    df.fillna(0)
+    # df['verification'] = df['verification'].map({'Verified': 0, 'Source Verified': 1, 'Not Verified': 2})
+    # df['grade'] = df['grade'].map({'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6})
+    # df['home_ownership'] = df['home_ownership'].map({'RENT': 0, 'OWN': 1, 'MORTGAGE': 2})
+    df['loan_status'] = df['loan_status'].map({'Charged Off': 0, 'Fully Paid': 1, 'Current': 2, 'In Grace Period': 3,
+                                               'Does not meet the credit policy. Status:Fully Paid': 1,
+                                               'Does not meet the credit policy. Status:Charged Off': 0,
+                                               'Late (31-120 days)': 4,
+                                               'Late (16-30 days)': 4,
+                                               'Issued': 5,
+                                               'Default': 0
+                                               })
+    df['term'] = df['term'].map(lambda s: int(s.split()[0]))
     df.to_csv('clean_data.csv', encoding='utf-8')
 
 
-def pd_to_np():
+def pd_to_matplot():
     dfc = pd.read_csv('clean_data.csv')
-    # df_asarray = dfc.values
-    # fig = plt.scatter(df_asarray[10], df_asarray[4])
-    # plt.set_xlabel('Credit History(yrs)')
-    # plt.set_ylabel('Credit Score')
-    # fig = plt.scatter(dfc["Bankruptcies"], dfc["Credit Score"], s=1)
-    # plt.xlabel("Bankruptcies")
-    # plt.ylabel("Credit Score")
-    # # fig.set_xlabel('Credit History(yrs)')
-    # # fig.set_ylabel('Credit Score')
+    # plt.xlabel('Education')
+    # plt.ylabel('Days past due')
     # plt.show(fig)
+    fig = plt.scatter(dfc["terms"], dfc["past_due_days"])
+    plt.show(fig)
 
 
 clean_training_data()
-print(pd_to_np())
+# print(pd_to_matplot())
